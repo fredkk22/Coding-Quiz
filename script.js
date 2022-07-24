175baf9755d2a6e1919f61b12e8cd12787ac72e8
fredkk22 <freddykwak123@gmail.com>
2022-07-23 20:23:04 -0400

    Added new page, edited index.html and script.js files
    Added a new page to the project for recording high scores and made sure the timer would begin decrementing when the quiz started, and a form section that I added to the index.html would pop up when time runs out.

--- script.js
+++ script.js
@@ -16,34 +16,32 @@ var ans2 = document.querySelector(".ans2")
 var ans3 = document.querySelector(".ans3")
 var ans4 = document.querySelector(".ans4")
 var ansChoices = document.querySelector(".anschoices");
-var homePageState = homePage.getAttribute("data-state");
-var quizBodyState = quizBody.getAttribute("data-state");
-
-
 var questionsList = "Question 1, Question 2".split(",");
 var ans1List = "Square Brackets, Arrays ".split(",");
 var ans2List = "Curly Braces, Cool".split(",");
 var ans3List = "Parentheses, Cool".split(",");
 var ans4List = "Semicolon, Pool".split(",");
-
 var index = 0;
+var correct = [".ans1", ".ans2"]
+var timeLeft = 60;
+var container = document.querySelector(".container")
+var endOfGame = document.querySelector(".endofgame")
+var form = document.querySelector(".endform")
 
 question.textContent = questionsList[index];
 ans1.textContent = ans1List[index];
 ans2.textContent = ans2List[index];
 ans3.textContent = ans3List[index];
 ans4.textContent = ans3List[index];
-var correct = [".ans1", ".ans3"]
 
 linkScores.textContent = "Hey";
 title.textContent = "UPenn LPS Coding Quiz (by Freddy Kwak)";
-timer.textContent = "timer";
+timer.textContent = "Time: " + timeLeft + " seconds remaining";
 startButton.textContent = "Start Quiz";
-ans1.textContent = "";
-ans2.textContent = "";
-ans3.textContent = "";
-ans4.textContent = "";
-question.textContent = "";
+form.textContent = "Enter your initials here:";
+
+container.removeChild(quizBody);
+container.removeChild(endOfGame);
 
 function navigate(next) {
     index = index + next;
@@ -53,12 +51,25 @@ function navigate(next) {
     ans2.textContent = ans2List[index];
     ans3.textContent = ans3List[index];
     ans4.textContent = ans3List[index];
+
+    container.appendChild(quizBody);
+}
+
+function beginTimer() {
+    var timeInterval = setInterval(function () {
+        timeLeft--;
+        timer.textContent = "Time:" + timeLeft + " seconds remaining";
+
+        if (timeLeft < 1) {
+            clearInterval(timeInterval);
+        }
+    }, 1000);
 }
 
 startButton.addEventListener("click", function () {
-    title.textContent = "";
-    start.textContent = "";
+    container.removeChild(homePage);
     navigate(0);
+    beginTimer();
 })
 
 ansChoices.addEventListener("click", function (event) {
@@ -69,18 +80,19 @@ ansChoices.addEventListener("click", function (event) {
         if (element.matches(correct[index])) {
             event.stopPropagation();
             navigate(1);
-            if (index = 5) {
-
+            if (timeLeft < 1) {
+                container.removeChild(quizBody);
+                container.appendChild(endOfGame);
+                timer.textContent = "Time ran out!"
             }
-        } else {
-            if (timeLeft = 0) {
 
+        } else {
+            timeLeft = timeLeft - 5;
+            if (timeLeft < 1) {
+                container.removeChild(quizBody);
+                container.appendChild(endOfGame);
+                timer.textContent = "Time ran out!"
             }
         }
-
-        
     }
-
-
-
 })
